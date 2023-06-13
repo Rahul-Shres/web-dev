e3ee42ab1eedf777a8a3db1bb8f4b58f36513491
rahul-shres <shrestharahul388@gmail.com>
2023-06-13 08:53:22 +0545

    never skip a day

---
+++ sikdai-ho-pasa/Backend/wallet_api/modules/users/controllers/userDashboard.js
@@ -0,0 +1,7 @@
+const userDashboard = (req, res) => {
+  res.status(200).json({
+    status: "this is dashboard",
+  });
+};
+
+module.exports = userDashboard;
--- sikdai-ho-pasa/Backend/wallet_api/modules/users/users.routes.js
+++ sikdai-ho-pasa/Backend/wallet_api/modules/users/users.routes.js
@@ -2,8 +2,13 @@ const express = require("express");
 const userRouter = express.Router();
 const userRegister = require("./controllers/userRegister");
 const userLogin = require("./controllers/userLogin");
-
+const userDashboard = require("./controllers/userDashboard");
+const auth = require("../../middleware/auth");
+//post
 userRouter.post("/register", userRegister);
 userRouter.post("/login", userLogin);
+userRouter.use(auth);
+//protected route
+userRouter.get("/dashboard", userDashboard);
 
 module.exports = userRouter;
