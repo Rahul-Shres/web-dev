4563f9440b2a0756b3727642acf047a512664966
rahul-shres <shrestharahul388@gmail.com>
2023-06-16 09:13:08 +0545

    sikdaixu pasa

--- sikdai-ho-pasa/Backend/wallet_api/middleware/auth.js
+++ sikdai-ho-pasa/Backend/wallet_api/middleware/auth.js
@@ -1,3 +1,29 @@
-const auth = (req, res, next) => {};
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  console.log(req.headers);
+
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    res.status(400).json({
+      status: "failed",
+      message: "Authorization failed! You must be logged in",
+    });
+    return;
+  }
+
+  const token = authorization.split("Bearer ")[1];
+
+  try {
+    const checkToken = jwt.verify(token, process.env.jwt_salt);
+    next();
+  } catch (error) {
+    res.status(400).json({
+      status: "failed",
+      message: "Authorization failed! Invalid Token",
+    });
+  }
+};
 
 module.exports = auth;
