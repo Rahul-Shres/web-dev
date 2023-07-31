3ed3ab3b866345f7970396037234c17de66dc5ad
Rahul-Shres <shrestharahul388@gmail.com>
2023-07-31 13:41:07 +0545

    ran into auth problem

--- 100-next-js-13-projects/promptopia/app/layout.jsx
+++ 100-next-js-13-projects/promptopia/app/layout.jsx
@@ -1,4 +1,5 @@
 import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 import '@styles/global.css';
 
 export const metadata = {
@@ -10,6 +11,8 @@ const RootLayout = ({children}) => {
   return (
     <html lang='en'>
       <body>
+        <Provider>
+
         {/* looks like background */}
         <div className='main'>
             <div className='gradient'></div>
@@ -19,9 +22,15 @@ const RootLayout = ({children}) => {
             <Nav />
             {children}
         </main>
+        </Provider>
       </body>
     </html>
   )
 }
 
 export default RootLayout
+// We use the Provider component in layout.js in Next.js
+//  to provide access to global state and data to all of 
+//  the child components. This means that any component that
+//   is rendered inside of the Provider component will be able 
+//   to access the same data, without having to pass it around manually.
--- 100-next-js-13-projects/promptopia/components/Provider.jsx
+++ 100-next-js-13-projects/promptopia/components/Provider.jsx
@@ -1,10 +1,12 @@
+"use client";
 
-const Provider = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import { SessionProvider } from "next-auth/react";
 
-export default Provider
+const Provider = ({ children, session }) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
+};
+
+export default Provider;
+// any component that is rendered inside of the Provider component
+// will be able to access the user's session information, such as
+// their username, email address, and whether or not they are currently logged in.
