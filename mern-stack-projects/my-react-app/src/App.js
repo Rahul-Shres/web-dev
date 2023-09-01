9a301a8a29dbc9c942485ea8e9fccd58a1c21d82
Rahul-Shres <shrestharahul388@gmail.com>
2023-09-01 13:00:55 +0545

    document title changer

---
+++ mern-stack-projects/my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DocumentTitle from './components/DocumentTitle';
+
+function App() {
+  return (
+    <div className="App">
+     <DocumentTitle />
+    </div>
+  );
+}
+
+export default App;
