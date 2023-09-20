af02098f80c45b4cb4d03983a4dd27af5ed8cc7d
Rahul-Shres <shrestharahul388@gmail.com>
2023-09-20 13:59:40 +0545

    pubg

--- mern-stack-projects/netflix-clone/api/routes/user.js
+++ mern-stack-projects/netflix-clone/api/routes/user.js
@@ -2,6 +2,7 @@
  const user = require('../models/User');
  const CryptoJs = require('crypto-js');
  const verify = require('../models/Verify');
+const User = require('../models/User');
 
  //UPDATE
 
@@ -19,12 +20,27 @@
                 req.params.id,
                 {
                     $set: req.body,
+                },{
+                    new : true
                 },
                 res.status(200).json(updateUser),
             );
             res.status(200).json(updateUser);
         }catch(err) {
-            res.status(500).json(err)
+            res.status(500).json("You can update only your account")
+        }
+        
+    }
+ });
+
+ //Delete
+ router.delete("/:id", verify, async(req,res)=>{
+    if(req.user.id === req.params.id || req.user.isAdmin){
+        try{
+            await User.findByIdAndDelete(req.params.id)
+            res.status(200).json("User has been deleted")
+        } catch(err) {
+            res.status(500).json("You can delete only your account");
         }
     }
  })
