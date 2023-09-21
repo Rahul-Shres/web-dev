4292f2af791343a676e365d3beffd1f3e39bae70
Rahul-Shres <shrestharahul388@gmail.com>
2023-09-21 13:09:35 +0545

    netflix and chill

--- mern-stack-projects/netflix-clone/api/routes/user.js
+++ mern-stack-projects/netflix-clone/api/routes/user.js
@@ -45,5 +45,68 @@ const User = require('../models/User');
     }
  })
 
+  //Get
+  router.get("/find/:id", async(req,res)=>{
+        try{
+            const user = await User.findById(req.params.id);
+            const { password, ...info} = user._doc;
+            res.status(200).json(info)
+        } catch(err) {
+            res.status(500).json("Cannot find your account");
+        }
+    }
+ )  
+
+//Get all
+router.get("/", verify, async(req,res)=>{
+    const query = req.query.new;
+    if( req.user.isAdmin){
+        try{
+            const users = query ? await User.find().sort({_id:10}).limit(2) : await User.find()
+            res.status(200).json(users)
+        } catch(err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed to see all the user`");
+
+    }
+ })
+
+// Get user stats
+router.get("/stats", verify, async (req, res) => {
+    const today = new Date();
+    const lastYear = new Date();
+    lastYear.setFullYear(today.getFullYear() - 1);
+  
+    // Array containing names of all months
+    const monthsArray = [
+      "January", "February", "March", "April",
+      "May", "June", "July", "August",
+      "September", "October", "November", "December"
+    ];
+  
+    try {
+      const data = await User.aggregate([
+        {
+          $project: {
+            month: { $month: '$createdAt' }
+          }
+        },
+        {
+          $group: {
+            _id: "$month",
+            total: { $sum: 1 }
+          }
+        }
+      ]);
+  
+      res.status(200).json({ monthsArray, data });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+
 
  module.exports = router
