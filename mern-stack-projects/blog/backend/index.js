8acd291f07ff7ff06a24b831d9ec2f1221a453bd
Rahul-Shres <shrestharahul388@gmail.com>
2023-10-02 13:04:47 +0545

    ejs

--- mern-stack-projects/blog/backend/index.js
+++ mern-stack-projects/blog/backend/index.js
@@ -7,8 +7,11 @@ dotenv.config();
 
 // Create an Express app
 const app = express();
+app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
 // Connect to MongoDB using Mongoose
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
@@ -28,12 +31,18 @@ dbConnection.once('open', () => {
 
 
 //GET API
-app.get("/",(req,res) => {
-  res.json({
-    status: 200,
-    message: "Success",
-  })
-})
+app.get("/", (req, res) => {
+  
+  
+  // Now, render the 'home' view
+  res.render('home');
+});
+app.get("/about", (req, res) => {
+  
+  // Now, render the 'home' view
+  res.render('about');
+});
+
 
 app.post('/createBlog', async (req,res)=>{
   const title = req.body.title;
@@ -52,6 +61,54 @@ app.post('/createBlog', async (req,res)=>{
     message: "Success",
   })
 })
+
+// Get All Blogs
+app.get('/blog', async (req, res) => {
+  try {
+    const blogs = await Blog.find();
+    if (blogs.length === 0) {
+      res.status(404).json({
+        message: 'Empty Blog'
+      });
+    } else {
+      res.status(200).json({
+        message: 'Blogs fetched successfully',
+        data: blogs
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: 'An error occurred while fetching blogs',
+      error: error.message
+    });
+  }
+});
+
+//Get Single Blog
+// Get Blog by ID
+app.get('/blog/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const blog = await Blog.findById(id);
+    if (blog) {
+      res.status(200).json({
+        message: 'Blog fetched successfully',
+        data: blog
+      });
+    } else {
+      res.status(404).json({
+        message: 'Blog not found'
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: 'An error occurred while fetching the blog',
+      error: error.message
+    });
+  }
+});
+
+
 // Start the Express server
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
