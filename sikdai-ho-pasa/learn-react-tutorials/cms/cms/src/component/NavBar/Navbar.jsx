81ba08d64b05d9e716e1c8cf214cdf0422f17259
Rahul-Shres <shrestharahul388@gmail.com>
2023-10-18 17:04:39 +0545

    cms project

---
+++ sikdai-ho-pasa/learn-react-tutorials/cms/cms/src/component/NavBar/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import './Navbar.css'; // Import your CSS file for styling
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="navbar-brand">
+          <a href="/" className="navbar-item">
+            Your Logo
+          </a>
+          <div
+            className={`navbar-burger${isOpen ? ' is-active' : ''}`}
+            onClick={toggleNavbar}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <div className={`navbar-menu${isOpen ? ' is-active' : ''}`}>
+          <div className="navbar-start">
+            <a href="/" className="navbar-item">
+              Home
+            </a>
+            <a href="/about" className="navbar-item">
+              About
+            </a>
+            <a href="/services" className="navbar-item">
+              Services
+            </a>
+            <a href="/contact" className="navbar-item">
+              Contact
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
