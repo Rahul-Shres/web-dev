ac0a5cf7aa386dd270bec6496235431e49cd1d7a
Rahul-Shres <shrestharahul388@gmail.com>
2023-11-17 18:05:13 +0545

    dynamic nav bar

--- sikdai-ho-pasa/learn-react-tutorials/fitbody/src/components/header/Header.jsx
+++ sikdai-ho-pasa/learn-react-tutorials/fitbody/src/components/header/Header.jsx
@@ -2,6 +2,25 @@ import React from 'react';
 import logo from '../../assets/img/dumble.png';
 
 const Header = () => {
+
+  const nav__links=[
+    {
+      path: "#",
+      display: "Home"
+    },
+    {
+      path: "#",
+      display: "Schedule"
+    },
+    {
+      path: "#",
+      display: "Classes"
+    },
+    {
+      path: "#",
+      display: "Pricing"
+    },
+  ]
   return (
     <div className="container">
       <div className="nav__wrapper">
@@ -9,7 +28,21 @@ const Header = () => {
           <div className="logo__img">
             <img src={logo} alt='Fitbody logo' />
             <h2>Fitbody</h2>
-            {/* continue from 3:43 */}
+            {/* Navigation Menu */}
+            <div className="navigation">
+              <ul className="menu">
+                {
+                  nav__links.map(items=>(
+                    <li className='nav__item'>
+                      <a href={items.path}>{items.display}</a>
+                    </li>
+                  ))
+                }
+              </ul>
+              {/* nav right */}
+
+              continue from 8:33
+            </div>
           </div>
         </div>
       </div>
