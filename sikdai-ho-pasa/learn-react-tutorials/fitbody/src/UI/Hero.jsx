96d68c3e1b93fca51941b61b7954255ab98f9c4a
Rahul-Shres <shrestharahul388@gmail.com>
2023-11-26 11:46:55 +0545

    Hero layout complete

--- sikdai-ho-pasa/learn-react-tutorials/fitbody/src/UI/Hero.jsx
+++ sikdai-ho-pasa/learn-react-tutorials/fitbody/src/UI/Hero.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import heroImg from "../assets/img/gym-02.png"; // Picture of a cute logo
-import dumbleImg from "../assets/img/dumble.png"; // Picture of a cute logo
+import heroImg from "../assets/img/gym-02.png";
+import dumbleImg from "../assets/img/dumble.png";
+import './Hero.css';
 
 const Hero = () => {
   return (
     <section className="hero">
       <div className="container">
         <div className="hero__wrapper">
-          {/* Hero Content */}
           <div className="hero__content">
-            <h2>Lorem ipsum dolor sit amet <span className="highlights">consectetur</span> adipisicing elit Adipisci tenetur.</h2>
+            <h2 className="section_title">
+              Lorem ipsum dolor sit amet <span className="highlights">consectetur</span> adipisicing elit Adipisci tenetur.
+            </h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat doloremque excepturi expedita!
-              Exercitationem reiciendis vero adipisci asperiores, ullam necessitatibus sed nesciunt? Nesciunt magni, sint
-              ut culpa porro voluptatibus nihil error nostrum mollitia numquam omnis ab sit quod illum id aliquam.
+              {/* Your paragraph content */}
             </p>
-
             <div className="hero__btns">
               <button className="register__btn">Get Started</button>
               <button className="watch__btn">
@@ -27,34 +26,28 @@ const Hero = () => {
             </div>
           </div>
 
-          {/* Hero Image */}
-          <div className="hero__img">
-            <div className="hero__img-wrapper">
-                <div className="box-01">
-                    <div className="box-02">
-                        <div className="box-03">
-                            <div className="box__img">
-                                <img src={heroImg} alt="" />
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="heart__rate">
-                        <h5>Heart Rate</h5>
-                        <span><i class="ri-heart-pulse-fill"></i></span>
-                        <h5>2567 BPM</h5>
-                    </div>
-                    <div className="gym__location">
-                        <h5>Heart Rate</h5>
-                        <span><i class="ri-map-pin-2-fill"></i></span>
-                        <h5>Find you a best anime</h5>
-                    </div>
-                    <div className="dumble__icon">
-                        <img src={dumbleImg} alt='' ></img>
-                    </div>
-
-
+          <div className="hero__img-wrapper">
+            <div className="box-01">
+              <div className="box-02">
+                <div className="box-03">
+                  <div className="box__img">
+                    <img className="box__img-src" src={heroImg} alt="" />
+                  </div>
                 </div>
+              </div>
+              <div className="heart__rate">
+                <h5>Heart Rate</h5>
+                <span><i className="ri-heart-pulse-fill"></i></span>
+                <h5>2567 BPM</h5>
+              </div>
+              <div className="gym__location">
+                <h5>Gym Location</h5>
+                <span><i className="ri-map-pin-2-fill"></i></span>
+                <h5>Find the best gym</h5>
+              </div>
+              <div className="dumble__icon">
+                <img className="dumble__img" src={dumbleImg} alt='' />
+              </div>
             </div>
           </div>
         </div>
