55c7cbe8f3ab9ed50f87748d2a8c0442b026a3ec
Rahul-Shres <shrestharahul388@gmail.com>
2023-11-26 15:16:44 +0545

    update and delete blog

--- mern-stack-projects/SajiloKhaana/backend/model/userModel.js
+++ mern-stack-projects/SajiloKhaana/backend/model/userModel.js
@@ -1,44 +1,43 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-const userSchema = new Schema({
-    userEmail: {
-        type: String,
-        required: [true, 'User email must be provided'],
-        unique: true // Consider adding unique constraint if emails should be unique
+const userSchema  =  new Schema({
+    userEmail : {
+        type : String,
+        required : [true,'userEmail must be provided'],
+  
     },
-    userPhoneNumber: {
-        type: String, // Storing phone numbers as strings
-        required: [true, "Phone number must be provided"]
+    userPhoneNumber : {
+        type : Number,
+        required : [true,"PhoneNumber must be provided"]
     },
-    userName: {
-        type: String,
-        required: [true, "Username must be provided"]
+    userName : {
+        type : String,
+        required  : [true,"UserName must be provided"]
     },
-    userPassword: {
-        type: String,
-        required: [true, "Password must be provided"],
-        minlength: 8,
-        // Consider using bcrypt to hash passwords before saving them
+    userPassword : {
+        type : String,
+        required : [true,"Password must be provided"],
+        minlength : 8,
+        // select : false
     },
-    role: {
-        type: String,
-        enum: ["customer", "admin"],
-        default: "customer",
+    role : {
+        type : String,
+        enum : ["customer","admin"],
+        default : "customer",
     },
-    otp: {
-        type: Number,
-        select: false // Hiding the OTP field by default
+    otp : {
+        type : Number,
+        select : false
     },
-    isOtpVerified: {
-        type: Boolean,
-        default: false,
-        select: false // Hiding the OTP verification status by default
+    isOtpVerified : {
+        type : Boolean,
+        default : false,
+        select : false
     }
-}, {
-    timestamps: true
-});
+},{
+    timestamps : true
+})
 
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
+const User = mongoose.model("User",userSchema)
+module.exports = User
--- sikdai-ho-pasa/node-js/beginner-pathway/backend/app.js
+++ sikdai-ho-pasa/node-js/beginner-pathway/backend/app.js
@@ -14,20 +14,17 @@ app.use(express.urlencoded({ extended: true }));
 
 // Render the home page with some example blog data
 app.get('/', async (req, res) => {
- //Table bata data nikalna paryo
-  const allBlogs = await blogs.findAll() // array ma data return garxa
-  // blogs vanne table bata sabai data dey vaneko
-  console.log(allBlogs);
+  const allBlogs = await blogs.findAll();
   const blogData = allBlogs.map(blog => ({
     id: blog.dataValues.id,
     title: blog.dataValues.title,
     subtitle: blog.dataValues.subTitle,
     content: blog.dataValues.content,
-    author: blog.dataValues.author, // Assuming there's an 'author' property in the blog data
-    publishedDate: blog.dataValues.createdAt // Assuming createdAt is in the desired format, otherwise, format it accordingly
+    author: blog.dataValues.author,
+    publishedDate: blog.dataValues.createdAt
   }));
 
-  res.render('blogs', { blogs: blogData });
+  res.status(200).json(blogData); // Sending JSON data
 });
 
 // Render the blog creation form
@@ -40,48 +37,81 @@ app.get("/createBlog", (req, res) => {
   res.render("createBlog");
 });
 
-// Table ma blog kasari halne
+// Create a new blog
 app.post("/createBlog", async (req, res) => {
   const { title, subtitle, content } = req.body;
 
   try {
-    // Create a new blog using the 'blogs' model
     const newBlog = await blogs.create({
       title: title,
       subTitle: subtitle,
       content: content
     });
 
-    console.log("New blog created:", newBlog); // Optional: log the created blog
+    console.log("New blog created:", newBlog);
 
-    res.redirect("/"); // Redirect to the home page after successful creation
+    res.redirect("/");
   } catch (error) {
     console.error("Error creating blog:", error);
     res.status(500).send("Error creating blog");
   }
 });
 
-//Table bata single blog kasari nikalne
+// Get a single blog
+app.get("/single/:id", async (req, res) => {
+  const id = req.params.id;
+  const blog = await blogs.findAll({
+    where: {
+      id: id
+    }
+  });
 
-// single blog page 
-app.get("/single/:id",async(req,res)=>{
-  const id = req.params.id
-  // second approach
-  // const {id} = req.params 
-  // id ko data magnu/find garnu paryo hamro table bata
-  const blog =  await blogs.findAll({
-      where : {
-          id : id
+  res.status(200).json(blog); // Sending JSON data
+});
+
+// Delete a blog
+app.get('/delete/:id', async (req, res) => {
+  const id = req.params.id;
+
+  await blogs.destroy({
+    where: {
+      id: id
+    }
+  });
+
+  res.status(200).json({ message: 'Blog deleted successfully' }); // Sending JSON response
+});
+
+// Edit a blog
+app.get('/edit/:id', async (req, res) => {
+  const id = req.params.id;
+  const blog = await blogs.findAll({
+    where: {
+      id: id
+    }
+  });
+
+  res.render('editBlog', { blog: blog });
+});
+
+// Update a blog
+app.post("/updateBlog/:id", async (req, res) => {
+  const id = req.params.id;
+  const { title, subTitle, content } = req.body;
+
+  await blogs.update({
+    title: title,
+    subTitle: subTitle,
+    content: content
+  },
+    {
+      where: {
+        id: id,
       }
-  })
-  // second finding approach
-  // const blog = await blogs.findByPk(id)
-    console.log(blog)
-      res.render("singleBlog",{blog:blog})
-  })
-  
+    });
 
-
+  res.status(200).json({ message: 'Blog updated successfully' }); // Sending JSON response
+});
 
 // Listen on port 8000
 app.listen(8000, () => {
