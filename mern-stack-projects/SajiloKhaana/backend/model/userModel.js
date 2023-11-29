e97c721b2f3f5f9a597614375792a8c51e41deeb
Rahul-Shres <shrestharahul388@gmail.com>
2023-11-29 12:53:48 +0545

    flexbox practice

--- mern-stack-projects/SajiloKhaana/backend/model/userModel.js
+++ mern-stack-projects/SajiloKhaana/backend/model/userModel.js
@@ -34,7 +34,10 @@ const userSchema  =  new Schema({
         type : Boolean,
         default : false,
         select : false
-    }
+    },
+    cart: [{
+        type: Schema.Types.ObjectId, ref: "Product",
+    }]
 },{
     timestamps : true
 })
