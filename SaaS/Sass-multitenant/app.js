1f99425b60191bd26e1aef55821f7993893713d4
Rahul-Shres <shrestharahul388@gmail.com>
2023-12-19 15:30:03 +0545

    google auth added

--- SaaS/Sass-multitenant/app.js
+++ SaaS/Sass-multitenant/app.js
@@ -1,14 +1,83 @@
-// REQUIRES START HERE
-const express = require('express');
-const app = express();
-require('dotenv').config();
-// REQUIRES ENDS HERE
 
-// DATABASE CONNECTION
-require('./model/index')
-app.get('/', (req, res) => {
-    res.send("I am live")
+// REQUIRES START HERE 
+const express = require("express")
+const app = express()
+require("dotenv").config()
+const passport = require("passport")
+// REQUIRES END HERE 
+
+// MIDDLEWARES 
+app.set("view engine","ejs")
+ app.use(passport.initialize())
+ app.use(passport.session())
+
+ passport.serializeUser(function(user,cb){
+    cb(null,user)  // cb(error,success) --> cb(error)
+ })
+ passport.deserializeUser(function(obj,cb){
+    cb(null,obj)
+ })
+
+
+// MIDDLEWARES END
+
+// database connection 
+require("./model/index")
+
+
+app.get("/",(req,res)=>{
+    res.render("home")
 })
-app.listen(process.env.PORT, ()=>{
-    console.log(`listening on ${process.env.PORT}`);
+
+
+// google login here 
+var userProfile ;
+let GoogleStrategy = require("passport-google-oauth").OAuth2Strategy
+
+passport.use( new GoogleStrategy({
+    clientID : process.env.GOOGLE_CLIENT_ID,
+    clientSecret : process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL : "http://localhost:9000/auth/google/callback"
+    // add the same url in Authorized redirect URIs in google developer console
+ 
+},
+function(accessToken,refreshToken,profile,done){
+ console.log(profile);
+ userProfile = profile  
+ return done(null,userProfile)
+
+}
+))
+
+app.get("/auth/google",passport.authenticate("google",{scope : ['profile','email']}) )
+
+app.get("/auth/google/callback",passport.authenticate("google",{
+    failureRedirect : "http://localhost:9000"
+}),
+function(req,res){
+    // check if google lay deko email already table ma exists xa ki nae vanerw  
+    // const users =  await users.findAll({
+        // where : {
+            const userEmail = userProfile.emails[0].value; // Accessing first email
+            
+        // }
+    // /
+
+    // xa 
+     // token generate garney ; jwt.sign
+
+    // xainw 
+     // users.create({})
+ res.send("Logged in successfully")
+}
+)
+
+// google login ends here 
+
+
+
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT,()=>{
+    console.log(`Server has started at port ${PORT}`)
 })
