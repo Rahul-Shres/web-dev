fc704f14b3720da3a13b2de3d01e758d1a34f6b2
Rahul-Shres <shrestharahul388@gmail.com>
2023-12-31 12:45:53 +0545

    add to cart, basic done

--- web-dev-projects/mern-stack-projects/shubha-yatra/frontend/src/App.jsx
+++ web-dev-projects/mern-stack-projects/shubha-yatra/frontend/src/App.jsx
@@ -1,22 +1,30 @@
-
-import './App.css'
-import {  RouterProvider } from 'react-router-dom'
-import router from './router'
-import Navbar from './global/components/navbar/Navbar'
-import Footer from './global/components/footer/Footer'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import Routes and Route
+import Navbar from './global/components/navbar/Navbar';
+import Footer from './global/components/footer/Footer';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import Home from './pages/home/Home';
+import Login from './pages/auth/login/Login';
+import Cart from './pages/cart/Cart';
 
 
 function App() {
-
-
   return (
     <>
-    <Navbar />
-   <RouterProvider router={router} />
-   <Footer />
-
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- web-dev-projects/mern-stack-projects/shubha-yatra/frontend/src/pages/home/components/Product.jsx
+++ web-dev-projects/mern-stack-projects/shubha-yatra/frontend/src/pages/home/components/Product.jsx
@@ -1,10 +1,14 @@
 import axios from "axios";
 import {useState, useEffect} from "react";
-
+// yo chayo
+import {useDispatch} from "react-redux"
+import { add } from "../../../store/cartSlice"
 
   
   export default function Product() {
-    const [product,setProducts] = useState([])
+    const [products,setProducts] = useState([])
+    // yo chayo
+    const dispatch = useDispatch()
 
     const fetchProducts = async () => {
       try {
@@ -23,6 +27,11 @@ import {useState, useEffect} from "react";
     useEffect(()=>{
       fetchProducts()
     },[])
+// yo chayo
+    const addToCart = (product)=>{
+      dispatch(add(product))
+   }
+
 
     return (
 <div className="relative w-full">
@@ -32,22 +41,23 @@ import {useState, useEffect} from "react";
          <h1 className="text-2xl font-bold text-yellow-900 md:text-3xl lg:w-10/12">Our Popular Foods</h1>
 
        <div className="flex flex-wrap justify-between">
- 
-          <div className="mx-auto overflow-hidden duration-300 transform bg-white rounded-lg shadow-md mt-11 w-80 dark:bg-slate-800 hover:scale-105 hover:shadow-lg">
+       {products.map((product) => (
+       <div key={product._id} className="mx-auto overflow-hidden duration-300 transform bg-white rounded-lg shadow-md mt-11 w-80 dark:bg-slate-800 hover:scale-105 hover:shadow-lg">
           <img className="object-cover object-center w-full h-48" src="https://images.unsplash.com/photo-1674296115670-8f0e92b1fddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Product Image" />
           <div className="p-4">
-            <h2 className="mb-2 text-lg font-medium text-gray-900 dark:text-white">Product Name</h2>
-            <p className="mb-2 text-base text-gray-700 dark:text-gray-300">Product description goes here.</p>
+            <h2 className="mb-2 text-lg font-medium text-gray-900 dark:text-white">{product.productName}</h2>
+            <p className="mb-2 text-base text-gray-700 dark:text-gray-300">{product.productDescription}</p>
             <div className="flex items-center">
-              <p className="mr-2 text-lg font-semibold text-gray-900 dark:text-white">$20.00</p>
-              <p className="text-base font-medium text-gray-500 line-through dark:text-gray-300">$25.00</p>
-              <button className="px-4 py-2 mx-6 font-bold text-white bg-yellow-500 rounded hover:bg-yellow-600">
+              <p className="mr-2 text-lg font-semibold text-gray-900 dark:text-white">{product.productPrice
+}</p>
+              <p className="text-base font-medium text-gray-500 dark:text-gray-300">{product.productStatus}</p>
+              <button onClick={()=>addToCart(product)} className="px-4 py-2 mx-6 font-bold text-white bg-yellow-500 rounded hover:bg-yellow-600">
     Add to Cart
   </button>
             </div>
           </div>
         </div>
-
+     ))}
    
        </div>
          </div>
