8f0bc3a1ec436ce5c24c391cfaf57193f4d5a74d
Rahul-Shres <shrestharahul388@gmail.com>
2024-01-02 11:39:30 +0545

    Dashboard, Question Page

--- SaaS/Sass-multitenant/controller/organizationController/organizationController.js
+++ SaaS/Sass-multitenant/controller/organizationController/organizationController.js
@@ -1,97 +1,117 @@
-const { QueryTypes } = require("sequelize");
-const { sequelize, users } = require("../../model");
+const { QueryTypes } = require("sequelize")
+const { sequelize, users } = require("../../model")
+
+exports.renderOrganizationForm = (req,res)=>{
+    res.render("addOrganization")
+}
+
+const generateRandomNumber = ()=>{
+    return Math.floor(1000+ Math.random() * 9000)
+}
+exports.createOrganization = async(req,res,next)=>{
+    const userId = req.userId
+
+    // find data of above userId 
+    const user = await users.findByPk(userId)
+  
+    const organizationNumber = generateRandomNumber()
+    const {organizationName,organizationAddress,organizationPhoneNumber,organizationEmail} = req.body 
+    const organizationPanNumber = req.body.organizationPanNumber || null 
+    const organizationVatNumber = req.body.organizationVatNumber|| null
+
+    // create users_org table 
+    await sequelize.query(`CREATE TABLE IF NOT EXISTS users_org(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, userId INT REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE, organizationNumber VARCHAR(255))`,{
+        type : QueryTypes.CREATE
+    })
+
+    // create organization Table 
+    await sequelize.query(`CREATE TABLE organization_${organizationNumber}(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), phoneNumber VARCHAR(255), address VARCHAR(255), panNo VARCHAR(255), vatNo VARCHAR(255) )`,{
+        type : QueryTypes.CREATE
+    })
+
+    // insert above data to table 
+    await sequelize.query(`INSERT INTO organization_${organizationNumber}(name,email,phoneNumber,address,panNo,vatNo) VALUES (?,?,?,?,?,?) `,{
+        type : QueryTypes.INSERT,
+        replacements  : [organizationName,organizationEmail,organizationPhoneNumber,organizationAddress,organizationPanNumber,organizationVatNumber]
+    })
+
+    await sequelize.query(`INSERT INTO users_org (userId,organizationNumber) VALUES(?,?)`,{
+        type : QueryTypes.INSERT,
+        replacements : [userId,organizationNumber]
+    })
+    user.currentOrgNumber = organizationNumber
+    await user.save()
+   req.organizationNumber  = organizationNumber
+   next()
+}
 
 
 
-exports.renderOrganizationForm = (req, res) => {
-  res.render("addOrganization");
-};
-// random number generate gara
-const generateRandomNumber = () => {
-  return Math.floor(1000 + Math.random() * 9000);
-};
-// naya organization create gara
-exports.createOrganization = async (req, res, next) => {
-  // user xa vane tesko id userId ko dabba ma rakha
-  const userId = req.userId;
+exports.createQuestionsTable = async(req,res,next)=>{
+    const organizationNumber   = req.organizationNumber
+    
+    // create table
 
-  // find data of above userId
-  const user = await users.findByPk(userId);
+    await sequelize.query(`CREATE TABLE question_${organizationNumber}(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,title VARCHAR(255),description TEXT, userId INT NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP )`,{
+        type : QueryTypes.CREATE
+    })
+    next()
+}
 
-  const organizationNumber = generateRandomNumber();
-  const {
-    organizationName,
-    organizationAddress,
-    organizationPhoneNumber,
-    organizationEmail,
-  } = req.body;
-  // yo duita null vaye ni hunxa
-  const organizationPanNumber = req.body.organizationPanNumber || null;
-  const organizationVatNumber = req.body.organizationVatNumber || null;
+exports.createAnswersTable = async(req,res)=>{
+    const organizationNumber = req.organizationNumber 
 
-  // create users_org table
-  await sequelize.query(
-    `CREATE TABLE IF NOT EXISTS users_org(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, userId INT REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE, organizationNumber VARCHAR(255))`,
-    {
-      type: QueryTypes.CREATE,
-    }
-  );
-
-  // create organization Table
-  await sequelize.query(
-    `CREATE TABLE organization_${organizationNumber}(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), phoneNumber VARCHAR(255), address VARCHAR(255), panNo VARCHAR(255), vatNo VARCHAR(255) )`,
-    {
-      type: QueryTypes.CREATE,
-    }
-  );
-
-  // insert above data to table
-  await sequelize.query(
-    `INSERT INTO organization_${organizationNumber}(name,email,phoneNumber,address,panNo,vatNo) VALUES (?,?,?,?,?,?) `,
-    {
-      type: QueryTypes.INSERT,
-      replacements: [
-        organizationName,
-        organizationEmail,
-        organizationPhoneNumber,
-        organizationAddress,
-        organizationPanNumber,
-        organizationVatNumber,
-      ],
-    }
-  );
-
-  await sequelize.query(
-    `INSERT INTO users_org (userId,organizationNumber) VALUES(?,?)`,
-    {
-      type: QueryTypes.INSERT,
-      replacements: [userId, organizationNumber],
-    }
-  );
-  user.currentOrgNumber = organizationNumber;
-    //  This action updates the currentOrgNumber field for this particular user in the database.
-    // you're updating the currentOrgNumber field (or property) of the user object and then using 
-    // await user.save(); to store that updated currentOrgNumber value in the user table within your database.
-  await user.save();
-  //  save the newly created organizationNumber to the currentOrgNumber field in the user table in the database. 
+    await sequelize.query(`CREATE TABLE answer_${organizationNumber}(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,userId INT NOT NULL REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE, answer TEXT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, questionId INT REFERENCES questions(id) ON DELETE CASCADE ON UPDATE CASCADE )`,{
+        type : QueryTypes.CREATE
+    })
+    res.redirect("/dashboard")
+}
 
 
-  // The line req.organizationNumber = organizationNumber; in the provided code snippet is setting a property 
-  // named organizationNumber on the req (request) object.
-  req.organizationNumber = organizationNumber;
-  next();
+
+
+// dashboard
+
+exports.renderDashboard = (req, res) => {
+  const currentUserRole = 'admin'; // Replace this with your logic to obtain the user role
+  res.render("dashboard/index", { currentUserRole });
 };
 
-exports.createForumTable = async (req, res) => {
-  const organizationNumber = req.organizationNumber;
 
-  // create table
+exports.renderForumPage = async(req,res)=>{
+    const organizatonNumber = req.user[0].currentOrgNumber
 
-  await sequelize.query(
-    `CREATE TABLE forum_${organizationNumber}(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,questions VARCHAR(255),answer VARCHAR(255))`,
-    {
-      type: QueryTypes.CREATE,
+    const questions = await sequelize.query(`SELECT * FROM question_${organizatonNumber}`,{
+        type : QueryTypes.SELECT
+    })
+    res.render("dashboard/forum",{questions : questions})
+}
+
+exports.renderQuestionForm = (req,res)=>{
+    res.render("dashboard/askQuestion")
+
+}
+
+
+exports.createQuestion = async (req,res)=>{
+   const organizationNumber = req.user[0].currentOrgNumber
+
+    const userId = req.userId
+  
+    const {title,description} = req.body 
+    if(!title || !description){
+        return res.send("Please provide title,description")
     }
-  );
-  res.send("Organization created successfully");
-};
+
+    // insert data into tables 
+    await sequelize.query(`INSERT INTO question_${organizationNumber} (title,description,userId) VALUES(?,?,?) `,{
+        type : QueryTypes.INSERT,
+        replacements : [title,description,userId]
+    })
+    res.redirect("/forum")
+
+}
+
+
+
+
--- SaaS/Sass-multitenant/routes/organizationRoute.js
+++ SaaS/Sass-multitenant/routes/organizationRoute.js
@@ -1,10 +1,12 @@
-const { createForumTable, createOrganization, renderOrganizationForm } = require("../controller/organizationController/organizationController")
+const { createOrganization, renderOrganizationForm, createQuestionsTable, createAnswersTable, renderDashboard, renderForumPage, renderQuestionForm, createQuestion } = require("../controller/organizationController/organizationController")
 const { isAuthenticated } = require("../middleware/isAuthenticated")
 
 
 const router = require("express").Router()
 
-router.route("/organization").get(renderOrganizationForm).post(isAuthenticated, createOrganization , createForumTable)
-
+router.route("/organization").get(renderOrganizationForm).post(isAuthenticated, createOrganization , createQuestionsTable, createAnswersTable)
+router.route('/dashboard').get(renderDashboard)
+router.route("/forum").get(isAuthenticated,renderForumPage)
+router.route("/question").get(isAuthenticated,renderQuestionForm).post(isAuthenticated,createQuestion)
 
 module.exports = router
