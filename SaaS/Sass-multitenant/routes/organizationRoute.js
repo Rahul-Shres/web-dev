128c6f76af48d606ab8b19288de1ebdb3c577655
Rahul-Shres <shrestharahul388@gmail.com>
2024-01-05 14:59:55 +0545

    Day 7(Answering Feature Implementation)

--- SaaS/Sass-multitenant/routes/organizationRoute.js
+++ SaaS/Sass-multitenant/routes/organizationRoute.js
@@ -1,4 +1,4 @@
-const { createOrganization, renderOrganizationForm, createQuestionsTable, createAnswersTable, renderDashboard, renderForumPage, renderQuestionForm, createQuestion, renderSingleQuestion, createQuestionImages } = require("../controller/organizationController/organizationController")
+const { createOrganization, renderOrganizationForm, createQuestionsTable, createAnswersTable, renderDashboard, renderForumPage, renderQuestionForm, createQuestion, renderSingleQuestion, createQuestionImages, answerQuestion } = require("../controller/organizationController/organizationController")
 const { isAuthenticated } = require("../middleware/isAuthenticated")
 const {multer,storage} = require('../middleware/multerConfig')
 const upload = multer({storage : storage})
@@ -14,4 +14,5 @@ router.route("/forum").get(isAuthenticated,renderForumPage)
 router.route("/question").get(isAuthenticated,renderQuestionForm).post(isAuthenticated,upload.array('questionImage'),createQuestion)
 
 router.route("/question/:id").get(isAuthenticated,renderSingleQuestion)
+router.route("/answer").post(isAuthenticated,answerQuestion)
 module.exports = router
