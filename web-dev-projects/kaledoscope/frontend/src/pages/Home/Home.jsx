7f1f66d455a326aa46534b569556e529b7ad1362
Rahul-Shres <shrestharahul388@gmail.com>
2024-01-27 10:18:19 +0545

    overlay added

--- web-dev-projects/kaledoscope/frontend/src/pages/Home/Home.jsx
+++ web-dev-projects/kaledoscope/frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import Appbar from '../../components/nabvar/Navbar';
+import Features from './features/features';
 
 const Home = () => {
   const iframeRef = useRef(null);
@@ -13,11 +14,25 @@ const Home = () => {
     <>
       <Appbar />
       <div style={{ position: 'relative', width: '100vw', height: '100vh', overflow: 'hidden', pointerEvents: 'none' }}>
+        {/* Color overlay */}
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            zIndex: 1,
+            backgroundColor: 'rgba(10, 250, 250, 0.5)', // Change the color and alpha value as needed
+          }}
+        ></div>
+
+        {/* YouTube iframe */}
         <iframe
           title="YouTube Video"
           width="100%"
           height="100%"
-          src="https://www.youtube.com/embed/jwggNJyI4JI?si=FlQPwtSHHiRvwWwT&autoplay=1"
+          src="https://www.youtube.com/embed/jwggNJyI4JI?si=FlQPwtSHHiRvwWwT&autoplay=1&mute=1&loop=1"
           frameBorder="0"
           allow="autoplay; encrypted-media"
           allowFullScreen
@@ -26,6 +41,7 @@ const Home = () => {
           style={{ position: 'absolute', top: 0, left: 0 }}
         ></iframe>
       </div>
+      <Features />
     </>
   );
 };
