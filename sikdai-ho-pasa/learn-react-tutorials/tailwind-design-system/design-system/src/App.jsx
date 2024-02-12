cedfade2d0ebd29c81a60e9822d1c142c627ced1
rahul.shre <shrestharahul388@gmail.com>
2024-02-12 16:34:36 +0545

    create framer motion component

--- sikdai-ho-pasa/learn-react-tutorials/tailwind-design-system/design-system/src/App.jsx
+++ sikdai-ho-pasa/learn-react-tutorials/tailwind-design-system/design-system/src/App.jsx
@@ -1,11 +1,27 @@
+import { motion } from "framer-motion"
+
+
 export default function App() {
+  const transition = {
+    duration: 1, // Adjust as desired (in seconds)
+    ease: 'easeOut', // Customize easing function if needed
+  };
   return (
     <><h1 className="text-3xl font-bold underline">
     Hello world!
   </h1>
   <h2>please check index.css file</h2>
     <button className="btn">Hover Me</button>
-    <input placeholder="email" type="text"/>
+    <motion.input 
+    whileHover={{ scale: 1.2 , rotateX: 180, rotateZ:20}}
+    transition={transition} 
+    onHoverStart={() => console.log('Hover starts')}
+    onHoverEnd={() => console.log('Hover Ends')}
+    placeholder="email" 
+    type="text"/>
+
+    <h2>To Do:</h2>
+    <p>I think we can create a component and pass height width, child to framer motion component</p>
   </>
     
 
---
+++ sikdai-ho-pasa/learn-react-tutorials/tailwind-design-system/design-system/src/animation.js
@@ -0,0 +1,6 @@
+export const myAnimation = {
+    transition: { duration: 1, ease: 'easeOut' },
+    variants: {
+      hover: { scale: 1.2, rotateX: 180, rotateZ: 20 },
+    },
+  };
