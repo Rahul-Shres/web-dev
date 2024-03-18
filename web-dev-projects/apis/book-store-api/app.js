bae5d65ec9a57389617f604c34e9feb37dba0b84
rahul.shre <shrestharahul388@gmail.com>
2024-03-18 16:43:31 +0545

    CRUD going

--- web-dev-projects/apis/book-store-api/app.js
+++ web-dev-projects/apis/book-store-api/app.js
@@ -1,7 +1,45 @@
 const express = require("express");
 const app = express()
+const { blogs } = require("./model/index")
 
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
 
+app.post('/createBlog', async(req,res)=>{
+    const {title, subtitle, description} = req.body
+    const newBlog = await blogs.create({
+        title,
+        subtitle,
+        description
+    })
+
+    res.send("form submitted successfully")
+})
+
+app.get('/blog', async(req,res)=>{
+    const allBlogs = await blogs.findAll()
+    console.log(allBlogs)
+    res.status(200).json({
+        message : "Blogs fetched successfully",
+        data : allBlogs
+    })
+})
+app.get('/single/:id', async(req,res)=>{
+    const {id} = req.params;
+
+    const singleBlog = await blogs.findAll({
+        where :{
+            id: id
+        }
+
+       
+    })
+    console.log(singleBlog)
+    res.status(200).json({
+        message : "Single Blog Fetched Successfully",
+        data : singleBlog
+    })
+})
 app.listen(3000, ()=>{
     console.log("I am live")
 })
--- web-dev-projects/mern/taskhero/src/App.jsx
+++ web-dev-projects/mern/taskhero/src/App.jsx
@@ -2,8 +2,15 @@ import Hero from "./pages/Hero";
 
 export default function App() {
   return (
+    <>
+    <div className="relative">
+      <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70"></div>
+      <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply  filter blur-xl opacity-70"></div>
+      <div className="absolute bottom-0 -left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply  filter blur-xl opacity-70"></div>
     <h1 className="text-3xl font-bold underline">
       <Hero />
     </h1>
+    </div>
+    </>
   )
 }
